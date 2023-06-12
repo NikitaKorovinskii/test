303f8b73bd5cb7fe04a04e00fe426f5c2b8d65fb
Nikitakorovinskii <kolbasa20022002@mail.ru>
2023-06-12 12:39:07 +0300

    goof

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component';
 import { TableCrudComponent } from './table-crud/table-crud.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from "@angular/forms";
 
 
 const appRoutes: Routes = [
@@ -15,12 +16,13 @@ const appRoutes: Routes = [
     AppComponent,
     TableCrudComponent,
   ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes),
-    BrowserAnimationsModule,
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes),
+        BrowserAnimationsModule,
+        FormsModule,
 
-  ],
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/table-crud/table-crud.component.ts
+++ src/app/table-crud/table-crud.component.ts
@@ -9,7 +9,9 @@ import {Router} from "@angular/router";
 export class TableCrudComponent implements OnInit  {
   pressureList: any[] = []
   idNum = 0;
-
+  p1: string = "";
+  p2: string = "";
+  p3: string = "";
   constructor(private route: Router) {}
   ngOnInit(): void {
     axios.get('https://8f652dd8-e67b-4d9b-a03a-fe63fe7857df.mock.pstmn.io/get').then(response => {
@@ -28,5 +30,26 @@ export class TableCrudComponent implements OnInit  {
   remove(id: number) {
     this.idNum = id-1
     console.log(this.idNum)
+  }
+
+  change(id: number,p1: string, p2: string,p3: string) {
+    this.pressureList = []
+    axios.post('https://8f652dd8-e67b-4d9b-a03a-fe63fe7857df.mock.pstmn.io/post', {
+      id: id,
+      p1: p1,
+      p2: p2,
+      p3: p3
+    })
+      .then(response => {
+        for (let i = 0; i < response.data.list.length; i++) {
+          this.pressureList.push(response.data.list[i]);
+        }
+      })
+
+
+    this.p1 = ""
+    this.p2 = ""
+    this.p3 = ""
+
   }
 }
