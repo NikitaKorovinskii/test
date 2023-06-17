546429aa9a1e3b2fc8e9304234ef725034ada850
kolba <kolbasa20022002@mail.ru>
2023-06-17 12:25:56 +0300

    lest v2.0

--- src/app/table-crud/table-crud.component.ts
+++ src/app/table-crud/table-crud.component.ts
@@ -1,48 +1,51 @@
 import {Component, OnInit} from '@angular/core';
 import axios from 'axios';
 import {Router} from "@angular/router";
+import {delay} from "rxjs";
 @Component({
   selector: 'app-table-crud',
   templateUrl: './table-crud.component.html',
   styleUrls: ['./table-crud.component.css']
 })
 export class TableCrudComponent implements OnInit  {
-  pressureList: any[] = []
+  entityList: any[] = []
   idNum = 0;
   p1: string = "";
   p2: string = "";
   p3: string = "";
   constructor(private route: Router) {}
   ngOnInit(): void {
-    this.pressureList = []
+    this.entityList = []
     axios.get('http://26.182.81.185:8080/main/getAllEntity1').then(response => {
       for (let i = 0; i < response.data.list.length; i++) {
-        this.pressureList.push(response.data.list[i]);
+        this.entityList.push(response.data.list[i]);
       }
     })
   }
 
-  edit(id: number) {
-  this.idNum = id-1
-    console.log(this.idNum)
-
-  }
-
+  //Удаление записи
   remove(id: number) {
-    this.idNum = id-1
-    console.log(this.idNum)
+    this.entityList = []
+    axios.delete('http://26.182.81.185:8080/main/deleteEntity1ById/'+id).then(response=> {
+      if (response.status == 200){
+        this.ngOnInit()
+      }
+      else {
+        alert("Ошибка!")
+      }
+    })
   }
 
-  change(p1: string, p2: string,p3: string) {
-
+  //Добавление записи
+  addEntity(p1: string, p2: string,p3: string) {
     axios.post('http://26.182.81.185:8080/main/addEntity1', {
       p1: p1,
       p2: p2,
       p3: p3
+    }).then(response=>{
+      console.log(response)
+      this.entityList.push(response.data)
     })
-
-    this.ngOnInit()
-
     this.p1 = ""
     this.p2 = ""
     this.p3 = ""
